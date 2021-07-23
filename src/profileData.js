72655be987d1e9f631c2c7b3ba11a284beb31761
JoKing <joebryant614@gmail.com>
2021-07-22 19:14:33 -0700

    data file added

---
+++ src/profileData.js
@@ -0,0 +1,73 @@
+const Joseph = {
+    details: 
+    {
+        name:"Joseph Bryant",
+        email: "bryantc0der@gmail.com",
+        location: "Mesa, AZ"
+    },
+    about: 
+    {
+        image: '',
+        body: ''
+    },
+
+    projects: 
+    [
+        {
+            title: '',
+            image: '',
+            description:'',
+            repo:'',
+            app:''
+        },
+        {
+            title: '',
+            image: '',
+            description:'',
+            repo:'',
+            app:''
+        },
+        {
+            title: '',
+            image: '',
+            description:'',
+            repo:'',
+            app:''
+        },
+        {
+            title: '',
+            image: '',
+            description:'',
+            repo:'',
+            app:''
+        },
+        {
+            title: '',
+            image: '',
+            description:'',
+            repo:'',
+            app:''
+        },
+        {
+            title: '',
+            image: '',
+            description:'',
+            repo:'',
+            app:''
+        },
+        {
+            title: '',
+            image: '',
+            description:'',
+            repo:'',
+            app:''
+        }
+    ],
+
+    resume:{
+        github:'',
+        linkedIn:''
+    }
+}
+
+export default Joseph
